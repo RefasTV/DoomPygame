0cd160a24f6a027d1206dce2b6a92d52f834de43
С Александр <sidorenkomaxsidorenko@ya.ru>
2023-05-03 15:06:59 +0300

    doom on walls

--- Game.py
+++ Game.py
@@ -11,36 +11,20 @@ white = (255,255,255)
 H = 1000 #Ширина
 W = 800 #Высота
 run = True
-#bullets = []
+bullets = []
 clock = pygame.time.Clock()
-numRays = 51
+numRays = 101
 screen = pygame.display.set_mode((H,W))
 tile = 400
 scaleX = H // numRays
 dist = numRays / (2 * math.tan(math.pi/3))
 FOV = math.pi / 3
 deltaAngle = FOV / numRays        # FOV / numRays
-pudge = pygame.image.load('pudge.png').convert_alpha() #download enemy pudge
 
+##########Run################
+pygame.init()
 
 ###########Classes###########
-class Enemy:
-    def __init__(self):
-        self.posX = 650
-        self.posY = 750
-        self.sprite = pudge
-        self.size = 50 # cube 50 x 50
-        self.baitDistance = 400
-        self.moveSlow = 500
-        #self.ray = Ray()
-
-    def MoveToPlayer(self):
-        if (abs(player.posX + player.posY - self.posX - self.posY) <= self.baitDistance):
-            self.posX, self.posY = self.posX + (player.posX - self.posX) / self.moveSlow, self.posY + (player.posY - self.posY) / self.moveSlow
-            print(self.posX, self.posY)
-
-
-
 class Ray:
     def __init__(self, angle):
         self.angle = angle
@@ -52,52 +36,31 @@ class Ray:
 
         pygame.draw.circle(screen, "green", GHP, 5)
 
-    def GetWallDepth(self):
-        lenX = W * math.cos(self.angle) / 100 #Parts of rayX (1/100)
-        lenY = W * math.sin(self.angle) / 100 #Parts of rayY (1/100)
+    def GetDepth(self):
+        lenX = W * math.cos(self.angle) / 100 #Parts of rayX (1/100) Части лучаХ (поделили его на 100 маленьких частей)  player.posX +
+        lenY = W * math.sin(self.angle) / 100 #Parts of rayY (1/100) player.posY +
 
         for i in range(1,100): # Founding the point by multiplying it on i
-            if (map.map[int((player.posY + lenY * i) // 100)][int((player.posX + lenX * i) // 100)] == 1): # if in block:
-                return (i) # return depth
-        return (200)
-    def GetEnemyDepth(self):
-        lenX = W * math.cos(self.angle) / 100 #Parts of rayX (1/100)
-        lenY = W * math.sin(self.angle) / 100 #Parts of rayY (1/100)
-
-        for i in range(1,100): # Founding the point by multiplying it on i
-            if ((int((player.posX + lenX * i) // 50),int((player.posY + lenY * i) // 50)) in EnemyesPos): # if in enemy:
-                return i
-        return (200)
+            if (len(map.map) > (player.posY + lenY * i )// 100 >= 0 and len(map.map[0]) > (player.posX + lenX * i) // 100 >= 0): # can check next string?
+                if (map.map[int((player.posY + lenY * i) // 100)][int((player.posX + lenX * i) // 100)] == 1): # if in block:
+                    return (i) # return depth
+        return (10000000)
     
     def GetHitPoint(self):
-        EnemyesPosXY = EnemyesPos
-        lenX = W * math.cos(self.angle) / 100 #Parts of rayX (1/100)
-        lenY = W * math.sin(self.angle) / 100 #Parts of rayY (1/100)
-        WallX, WallY, EnemyX, EnemyY = 0,0,0,0
-        for i in range (1,100): # Founding the point by multiplying it on i
-            if (len(map.map) > (player.posY + lenY * i )// 100 >= 0 and len(map.map[0]) > (player.posX + lenX * i) // 100 >= 0):
-                if (map.map[int((player.posY + lenY * i) // 100)][int((player.posX + lenX * i) // 100)] == 1): # if in block
-                    return (player.posX + lenX * i, player.posY + lenY * i,EnemyX,EnemyY) # return points
-            if ((int((player.posX + lenX * i) // 50),int((player.posY + lenY * i) // 50)) in EnemyesPosXY): # if in enemy:
-                EnemyesPosXY.remove((int((player.posX + lenX * i) // 50),int((player.posY + lenY * i) // 50)))
-                EnemyX, EnemyY = player.posX + lenX * i, player.posY + lenY* i
-        return (WallX,WallY,EnemyX,EnemyY)
-    '''
-    def GetHitPointEnemy(self):
-        lenX = W * math.cos(self.angle) / 50#100 #Parts of rayX (1/100)
-        lenY = W * math.sin(self.angle) / 50#100 #Parts of rayY (1/100)
+        lenX = W * math.cos(self.angle) / 100 #Parts of rayX (1/100) Части лучаХ (поделили его на 100 маленьких частей)  player.posX +
+        lenY = W * math.sin(self.angle) / 100 #Parts of rayY (1/100) player.posY +
 
-        for i in range(1,50): # 100 Founding the point by multiplying it on i
-            if (map.map[int((player.posY + lenY * i) // 100)][int((player.posX + lenX * i) // 100)] == 1): # if in block
-                return (0,0) # return points
-            if ((int((player.posX + lenX * i) // 50),int((player.posY + lenY * i) // 50)) in EnemyesPos): # if in enemy:
-                EnemyesPos.remove((int((player.posX + lenX * i) // 50),int((player.posY + lenY * i) // 50)))
-                return (player.posX + lenX * i, player.posY + lenY* i) # return point
-
+        for i in range(1,100): # Founding the point by multiplying it on i
+            if (len(map.map) > (player.posY + lenY * i )// 100 >= 0 and len(map.map[0]) > (player.posX + lenX * i) // 100 >= 0): # can check next string?
+                if (map.map[int((player.posY + lenY * i) // 100)][int((player.posX + lenX * i) // 100)] == 1): # if in block:
+                    return (player.posX + lenX * i, player.posY + lenY* i) # return point
         return (0,0)
-    '''
 
-'''
+    def GetDistance(self):
+        x,y = self.GetHitPoint()
+        return (x - player.posX,y - player.posY)
+
+
 class Bullet:
     def __init__(self):
         self.angle = player.angle
@@ -110,7 +73,7 @@ class Bullet:
     def draw(self):
         if (W > self.curX > 0) and (H > self.curY > 0):
             pygame.draw.circle(screen,'white',self.nextPos(),3)
-'''
+
 
 class Map:
     def __init__(self):
@@ -122,7 +85,7 @@ class Map:
             [1,'_','_','_', 1,  1,    '_', '_','_',1],
             [1,'_','_','_','_', '_',  '_', 1,'_',1],
             [1,'_' ,'_', '_','_', '_','_', 1,'_',1],
-            [1,'_' ,'_', '_','_','enemy','_', 1,'_',1],
+            [1,'_' ,'_', '_','_','_','_', 1,'_',1],
             [1, 1,  1,  1,'_','_', '_',  1,  1, 1],
             [1,'_','_','_','_','_', '_','_','_',1],
             [1,'_','_','_',1,'_', '_',  '_','_',1],
@@ -136,20 +99,20 @@ class Map:
 
         self.colorMap = [
             [orange, orange,  orange,  orange,  orange,  orange,  orange,  orange, orange, orange],
-            [orange, '_',   '_',      '_',     '_',     '_',     '_',      '_',     '_',    orange],
-            [orange, '_',   '_',      '_',     '_',     '_',     '_',      '_',     '_',    orange],
-            [orange, '_',   '_',      '_',     green,    green,    '_',    '_',     '_',    orange],
-            [orange, '_',   '_',      '_',     green,    green,    '_',    '_',     '_',    orange],
+            [orange, '_',   '_',      '_',     '_',     '_',     '_',     '_',     '_',    orange],
+            [orange, '_',   '_',      '_',     '_',     '_',     '_',     '_',     '_',    orange],
+            [orange, '_',   '_',      '_',     green,    green,    '_',     '_',     '_',    orange],
+            [orange, '_',   '_',      '_',     green,    green,    '_',     '_',     '_',    orange],
             [orange, '_',   '_',      '_',     '_',     '_',     '_',     green,    '_',    orange],
             [orange, '_' ,  '_',      '_',     '_',     '_',     '_',     green,    '_',    orange],
             [orange, '_' ,  '_',      '_',     '_',     '_',     '_',     green,    '_',    orange],
-            [orange, orange,orange,  orange,     '_',     '_',     '_',   green,  green,    orange],
+            [orange, orange,orange,  orange,     '_',     '_',     '_',     green,  green,    orange],
             [orange, '_',   '_',      '_',     '_',     '_',     '_',     '_',     '_',    orange],
             [orange, '_',   '_',      '_',    orange,     '_',     '_',     '_',     '_',    orange],
-            [orange, '_',   orange,   orange,   orange,   green,   green,     '_',     '_',    orange],
-            [orange, '_',   '_',      '_',     orange,    '_',     green,     '_',     '_',    orange],
+            [orange, '_',   orange,   orange,   orange,     green,   green,     '_',     '_',    orange],
+            [orange, '_',   '_',      '_',     orange,     '_',     green,     '_',     '_',    orange],
             [orange, '_',   '_',      '_',     orange,     '_',     '_',     '_',     '_',    orange],
-            [orange, '_',  orange,  '_',     '_',     '_',     '_',     orange,     orange,    orange],
+            [orange, '_',   orange,      '_',     '_',     '_',     '_',     orange,     orange,    orange],
             [orange, '_',   '_',      '_',     '_',     '_',     '_',     '_',     '_',    orange],
             [orange, orange, orange,   orange,  orange,  orange,  orange,  orange,  orange,  orange],
         ]
@@ -160,43 +123,29 @@ class Map:
                 if (self.map[j][i] == 1):
                     pygame.draw.rect(screen,'darkgray',(i*100, j*100, 100, 100))
 
-
     def newDraw(self): # Draw 3D
         for curRay in range(numRays):
-            wallx, wally,enemyx, enemyy = rays[curRay].GetHitPoint()
-            if (wallx,wally) != (0,0):
-                proj_coff = dist * tile
-                depth = rays[curRay].GetWallDepth() 
-                proj_h = proj_coff / depth
-                depth *= math.cos(player.angle - rays[curRay].angle) # Remove Fisheye
-
-
-                c1,c2,c3 = self.colorMap[int(wally//100)][int(wallx//100)]
-                if (c1 - depth * 2 < 0):
-                    c1 = 0
-                else:
-                    c1 -= depth * 2
-                if (c2 - depth * 2 < 0):
-                    c2 = 0
-                else:
-                    c2 -= depth * 2
-                if (c3 - depth * 2 < 0):
-                    c3 = 0
-                else:
-                    c3 -= depth * 2
-                if (c1 > 15 or c2 > 15 or c3 > 15):
-                    pygame.draw.rect(screen, [c1, c2, c3],
-                                    (scaleX * curRay, W / 2 - proj_h // 2, scaleX, proj_h))
-                    
-            if (enemyx,enemyy) != (0,0):
-                depth = rays[curRay].GetEnemyDepth()
-                scaleSize = depth / 2
-                if (scaleSize < 0):
-                    scaleSize = 0
-                elif (scaleSize > 50):
-                    scaleSize = 50
-                newPudge = pygame.transform.scale(pudge,((50-scaleSize) * 10, (50-scaleSize) * 10))
-                screen.blit(newPudge, (curRay * 10, W // 2))
+            GHPx,GHPy = rays[curRay].GetHitPoint()
+            proj_coff = dist * tile
+            depth = rays[curRay].GetDepth() 
+            proj_h = proj_coff / depth
+            depth *= math.cos(player.angle - rays[curRay].angle)
+            c1,c2,c3 = self.colorMap[int(GHPy//100)][int(GHPx//100)]
+            if (c1 - depth*3 < 0):
+                c1 = 0
+            else:
+                c1 -= depth * 3
+            if (c2 - depth * 3< 0):
+                c2 = 0
+            else:
+                c2 -= depth * 3
+            if (c3 - depth * 3< 0):
+                c3 = 0
+            else:
+                c3 -= depth * 3
+            pygame.draw.rect(screen, [c1, c2, c3],
+                            (scaleX * curRay, W / 2 - proj_h // 2, scaleX, proj_h))
+            
         
     def MiniMap(self):
         for j in range(len(map.map)):
@@ -269,35 +218,38 @@ def TwoD():
 
 def ThreeD():
     map.newDraw()
-    #map.MiniMap()
+    map.MiniMap()
 
 
 ##########Class Objects##########
 player = Player()
 map = Map()
 rays = [0] * numRays
-enemyes = [Enemy()]
-EnemyesPos = []
-
-##########Run################
-pygame.init()
 
 ######### Game Loop##############
 while run:
-
     clock.tick(600)
     for i in range(numRays):
+        '''
+        if (i == 0):
+            rays[i] = Ray(player.angle - player.cameraSens * 20)
+        if i == 1:
+            rays[i] = Ray(player.angle - player.cameraSens * 10)
+        if i == 2:
+            rays[i] = Ray(player.angle)
+        if i == 3:
+            rays[i] = Ray(player.angle + player.cameraSens * 10)
+        if i == 4:
+            rays[i] = Ray(player.angle + player.cameraSens * 20)
+        '''
+
+    ##################### FIX ##################### 
         if i == numRays / 2 - 0.5:
             rays[i] = Ray(player.angle)
         if i < numRays / 2:
             rays[i] = Ray(player.angle - FOV/numRays * (numRays/2 - i))
         if i > numRays / 2:
             rays[i] = Ray(player.angle + FOV/numRays * (i - numRays/2))
-
-
-    for enem in enemyes:
-        EnemyesPos.append((int(enem.posX // 50), int(enem.posY // 50)))
-
 
     pygame.display.set_caption(f'{clock.get_fps() : .1f}')
     for ev in pygame.event.get():
@@ -307,9 +259,7 @@ while run:
         if (ev.type == pygame.MOUSEBUTTONDOWN): #Shoot
             player.Shoot()
         """
-    for i in range(len(enemyes)):
-        enemyes[i].MoveToPlayer()
-    #print(player.posX, player.posY)
+
     #Game
     screen.fill((0,0,0))
     player.movement()
@@ -319,7 +269,7 @@ while run:
     #TwoD()
     ThreeD()
 
-    #screen.blit(pudge,(0,0))
+
     '''
     DrawBullets()
     if (len(bullets) > 25): #Bullets Optimization
